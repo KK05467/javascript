5501bd219105e6d56fe8c869bddc9bae09dfb152
KEERTAN KUMAR <keertan2005@gmail.com>
2025-11-04 16:10:34 +0000

    math  and num

---
+++ 01_basics/06_nums_maths.js
@@ -0,0 +1,35 @@
+const score = 400
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(1));
+
+const otherNumber = 23.56487564;
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+
+
+
+//**********************************Maths********************************************
+// console.log(Math);
+// console.log(Math.abs(-1))
+// console.log(Math.round(4.6))
+// console.log(Math.round(4.4))
+
+console.log(Math.random()); //Any random value between 0 and 1
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max-min + 1)) + min)
