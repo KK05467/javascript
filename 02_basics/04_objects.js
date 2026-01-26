62bfdf3793923b8adf82faed3c1e6b5d248e8c99
Keertan <keertan2005@gmail.com>
2026-01-26 16:54:29 +0530

    json talk

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,43 +1,60 @@
-//const tinderUser = newObject()  //this is singleton object
-const tinderUser= {} //this is non-singleton object
+// //const tinderUser = newObject()  //this is singleton object
+// const tinderUser= {} //this is non-singleton object
 
 
-tinderUser.id = "123abc"
-tinderUser.name = "Sammy"
-tinderUser,isLoggedIn = false
+// tinderUser.id = "123abc"
+// tinderUser.name = "Sammy"
+// tinderUser,isLoggedIn = false
 
 
-// console.log(tinderUser);
+// // console.log(tinderUser);
 
-// const regularUser = {
-//     email: "some@gmail.com",
-//     fullname: {
-//         userfullname: {
-//             firstname: "keertan",
-//             lastname: "kumar"
-//         }
+// // const regularUser = {
+// //     email: "some@gmail.com",
+// //     fullname: {
+// //         userfullname: {
+// //             firstname: "keertan",
+// //             lastname: "kumar"
+// //         }
         
-//     }
+// //     }
+// // }
+
+// // console.log(regularUser.fullname.userfullname.firstname);
+
+// const obj1 = {
+//     1: "a",
+//     2: "b"
 // }
 
-// console.log(regularUser.fullname.userfullname.firstname);
+// const obj2 = {
+//     3: "a",
+//     4: "b"
+// }
 
-const obj1 = {
-    1: "a",
-    2: "b"
+// //onst obj3 = {obj1, obj2}
+
+// const obj3 = Object.assign({}, obj1, obj2)
+// //console.log(obj3);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+const course = {
+    coursename:"js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
 }
 
-const obj2 = {
-    3: "a",
-    4: "b"
-}
+//course.courseInstructor
 
-//onst obj3 = {obj1, obj2}
+const {courseInstructor: instructor} = course
 
-const obj3 = Object.assign({}, obj1, obj2)
-//console.log(obj3);
+// console.log(courseInstructor);
+console.log(instructor);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.entries(tinderUser));
+//backend provides the info through api i form of json file which is nothing but just like an object with no name and keys as strings. It can be fetched and can be used through fetch function
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
