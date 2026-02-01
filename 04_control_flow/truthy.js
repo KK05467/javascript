7a7beb6231d6f55edbf8b997c3ccc2032eafd93c
Keertan <keertan2005@gmail.com>
2026-02-01 13:09:13 +0530

    Nco

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -13,3 +13,34 @@ else{
 //*************Falsy values****************
 
 //false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+
+// if(userEmail.length === 0){
+//     console.log("Array is empty");
+// }
+
+const emptyObj = {}
+    if(Object.keys(emptyObj).length === 0){
+        console.log("Object is empty")
+    }
+
+
+// Nullish Coalescing Operator (??) : null undefined
+
+let val1;
+//val1 = 5 ?? 10
+
+//val - null ?? 15
+
+// val1 = undefined ?? 15
+
+val1 = null ?? 10 ?? 10
+
+
+console.log(val1);
+
+
+//Ternary Operator
+
+// condition ? true : false
+
