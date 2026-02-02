6197779ed7b84c951578053fe692f22bd3afdeda
Keertan <keertan2005@gmail.com>
2026-02-02 21:35:52 +0530

    for loop

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -12,9 +12,26 @@ for(let i = 0; i < 10; i++){
     //console.log(element);
 }
 
-for(let i = 0; i<= 10; i++){
-    console.log(`Outer loop value: ${i}`);
-    for(let j = 0; j<= 10; j++){
-        console.log(`Inner loop value ${j} and inner loop ${i}`)
+// for(let i = 0; i<= 10; i++){
+//     console.log(`Outer loop value: ${i}`);
+//     for(let j = 0; j<= 10; j++){
+//         console.log(`Inner loop value ${j} and inner loop ${i}`)
+//     }
+// }
+
+
+
+// break and continue
+
+for(let i = 1; i<=20; i++){
+
+    // if(i == 5) {
+    //     console.log(`Detected 5`);
+    //     break   //breaks after current iteration
+    // }
+    if(i == 5) {
+        console.log(`Detected 5`);
+        continue   //skips the current iteration
     }
+    console.log(`Value of i is ${i}`);
 }
